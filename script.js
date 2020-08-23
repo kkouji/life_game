475fd0ed197550b80ad259a771e90b5302250612
chio_kkk <anananfare@gmail.com>
2020-08-23 17:13:21 +0900

    canvasの使い方試行錯誤

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const WIDTH = 600;
+const HEIGHT = 600;
+const interval = 30;
+var is_update;
+var frame;
+
+window.onload = function () {
+  var canvas = document.getElementById('field');
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  is_update = true;
+  frame = setInterval("sample()",interval);
+}
+
+function startstop() {
+  if (is_update) {
+    clearInterval(frame);
+  }else{
+    frame = setInterval("sample()",interval);
+  }
+  is_update = !is_update;
+}
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
@@ -7,22 +30,24 @@ function getRandomInt(max) {
 function sample() {
   //描画コンテキストの取得
   var canvas = document.getElementById('field');
-  canvas.width = 600;
-  canvas.height = 300;
+
   if (canvas.getContext) {
 
     var context = canvas.getContext('2d');
+    context.clearRect(0,0,canvas.width/2,canvas.height);
     context.fillStyle = 'rgb(255,00,00)'; //塗りつぶしの色は赤
     // context.arc(15,20,1,0,Math.PI*2,true);
 
-    for(let i = 1 ; i < 10 ; i++){
-      // context.fillRect(10*i,10*i,1*i,1*i);
-      var tate = getRandomInt(300);
-      var yoko = getRandomInt(600);
-      console.log(i,"yoko",yoko,"tate",tate);
+    var num = getRandomInt(300);
 
-      context.fillRect(yoko,tate,1*i,1*i);
-      context.fillText(i,yoko,tate);
+    for(let i = 1 ; i < num ; i++){
+      // context.fillRect(10*i,10*i,1*i,1*i);
+      var tate = getRandomInt(HEIGHT);
+      var yoko = getRandomInt(WIDTH);
+      // console.log("num",num,"iter",i,"yoko",yoko,"tate",tate);
+
+      context.fillRect(yoko,tate,1,1);
+      // context.fillText(i,yoko,tate);
     }
 
   }
